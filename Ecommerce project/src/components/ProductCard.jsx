74cb1357d4eeceba029e8c3dee01af7a34371028
afaqkhanniazi <afaqkhan.ak2006gmail.com>
2024-11-06 11:36:40 +0500

    ecommerce project

---
+++ Ecommerce project/src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+import img from '../assets/images/game.png';
+
+export default function ProductCard(){
+    return(
+        <div className="border-2 border-red-500 w-64 h-[350px]">
+            <img src="img" alt="" />
+            Hello</div>
+        
+    )
+}
