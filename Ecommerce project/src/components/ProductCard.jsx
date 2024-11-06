a304b18273cf28d8ea605319003d824dc314c52d
afaqkhanniazi <afaqkhan.ak2006gmail.com>
2024-11-06 11:50:48 +0500

    ecommerce project

--- Ecommerce project/src/components/ProductCard.jsx
+++ Ecommerce project/src/components/ProductCard.jsx
@@ -1,10 +1,15 @@
-import img from '../assets/images/game.png';
+import img from "../assets/images/game.png";
 
 export default function ProductCard(){
     return(
         <div className="border-2 border-red-500 w-64 h-[350px]">
-            <img src="img" alt="" />
-            Hello</div>
+            <img src={img} alt="Product img" />
+            <h3>HAVIT HV-G92 Gamepad</h3>
+            <p>
+            <span>$120</span>
+            <span>$160</span>
+            </p>
         
+            </div>  
     )
 }
