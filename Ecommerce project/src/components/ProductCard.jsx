ea6c2571fee031ac438eea6425f06fb1ece14b0d
afaqkhanniazi <afaqkhan.ak2006gmail.com>
2024-12-02 11:54:40 +0500

    prject

--- Ecommerce project/src/components/ProductCard.jsx
+++ Ecommerce project/src/components/ProductCard.jsx
@@ -1,12 +1,14 @@
 import img from "../assets/images/game.png";
+import ReactStars from 'react-stars'
 
 export default function ProductCard(props){
 
-    const discountPrice =  (props.discountPercentage) - props.price 
-    console.log('discountPrice', discountPrice);
+    const discountPrice =
+    (props.price - props.discountPercentage * (props.price / 100 )).toFixed(2);
+    
     
     return(
-        <div className=" group border-2 flex gap-4 flex-col relative bg-[F5F5F5] w-64 h-[350px]">
+        <div className="group p-3 drop-shadow-md border-2 flex gap-4 flex-col relative bg-[F5F5F5] w-64 main-h-[350px]">
             <div className="discount absolute top-3 left-3 bg-primary w-14 h-6 rounded flex items-center text-xs justify-center text text-white">-40%</div>
             <div className="bdr">
             <img src={props.image} 
@@ -15,12 +17,22 @@ export default function ProductCard(props){
             </div>
             <div>
             <h3 className="font-medium group-hover:text-primary transition-all duration-300">HAVIT HV-G92 Gamepad</h3>
-            <p>
-            <span>${discountPrice}</span>
-            {" "}
-            <span>${props.price}</span>
+            <p className="font-medium">
+            <span className="text-primary">${discountPrice}</span>{" "}
+            <span className="text-gray-400 line-through">${props.price}</span>
             </p>
-            <p>stars</p>
+            
+            <div>
+            <ReactStars
+            count={5}
+            value={props.rating}
+            //   onChange={ratingChanged}
+            size={24}
+            color2={'#db4444'} 
+            edit={false}/>,
+                
+                
+            </div>
             </div>
             </div>  
     )
--- Ecommerce project/src/pages/Products.jsx
+++ Ecommerce project/src/pages/Products.jsx
@@ -18,19 +18,54 @@ export default function Products() {
     getProductData();
   }, []);
 
-  console.log("products ", products);
+  // console.log("products ", products);
+
+  const [searchTerm,setsearchTerm] = useState (" ")
+
+  console.log(searchTerm)
+
+  const searchProducts = ()=>{
+  const result =  products?.filter=((item)=>{
+    return item.price > 50;
+    });
+    console.log("result",result);
+    return result  || []
+    
+  };
+  const searchkaResult = searchProducts();
 
   return (
     <div className="container-x">
+
+      <input 
+      type="search" 
+      name="price" id="price" 
+      onChange={(event)=>{
+
+      // console.log("value",event.target.value,);
+
+      setsearchTerm(event.target.value)
+      
+
+      }}
+      className="block w-full outline-none rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary my-8 sm:text-sm/6" 
+      placeholder="Search your product"
+      />
+
+
       {products === null ? "Loading...." : null}
 
       <div className="flex flex-wrap justify-center gap-4">
-        {products?.map((item) => (
-          <ProductCard key={item.id} image={item.thumbnail} name={item.title}
+        {searchkaResult?.map((item) => (
+          <ProductCard 
+          key={item.id} 
+          image={item.thumbnail} 
+          name={item.title}
           price={item.price} 
-          discountPercentage={item.discountPercentage}/>
+          discountPercentage={item.discountPercentage}
+          rating={item.rating}/>
         ))}
       </div>
-    </div>
+    </div> 
   );
 }
