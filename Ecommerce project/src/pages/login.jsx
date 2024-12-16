0f0393487e21a5d3d25168a29372c836aa62bea4
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2024-12-16 10:19:04 +0500

    project

--- Ecommerce project/src/pages/login.jsx
+++ Ecommerce project/src/pages/login.jsx
@@ -1,9 +1,48 @@
 
 export default function login() {
+
+  const formSubmitHua = (event)=>{
+    event.preventDefault()
+    console.log("testing"new FormData(event.target);
+    
+  }
   return (
-    <div>login page
-        <input className="border-2 border-primary" type="email" />
-        <input className="border-2 border-primary" type="password" />
+    <div className="center h-[calc(100vh - 160px)]">
+    <form onSubmit={formSubmitHua} className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col w-full">
+      <h2 className="text-gray-900 text-lg font-medium title-font mb-5">
+        Login
+        </h2>
+      <div class="relative mb-4">
+        <label 
+        htmlFor="email" 
+        className="leading-7 text-sm text-gray-600">
+          Email
+          </label>
+        <input 
+        type="email" 
+        id="email" 
+        name="email"
+        required 
+        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <div className="relative mb-4">
+        <label htmlFor="password" 
+        className="leading-7 text-sm text-gray-600">
+          Password
+          </label>
+        <input 
+        type="password" 
+        id="password" 
+        name="password" 
+        required
+        className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+      </div>
+      <button 
+      className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        Login
+        </button>
+      <p className="text-xs text-gray-500 mt-3">Literally you probably haven't heard of them jean shorts.</p>
+    </form>
    </div>
   )
 }
