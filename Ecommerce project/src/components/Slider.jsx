7c0099aa8bb23c2a6e8f16354110795014198498
RehmanKhan007 <156161992+RehmanKhan007@users.noreply.github.com>
2025-01-01 09:28:53 +0500

    search new arriwal fix

--- Ecommerce project/src/components/Slider.jsx
+++ Ecommerce project/src/components/Slider.jsx
@@ -1,3 +1,62 @@
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// import {
+//   Navigation,
+//   Pagination,
+//   Scrollbar,
+//   A11y,
+//   Autoplay,
+// } from "swiper/modules";
+// import ProductCard from "./ProductCard";
+// import useproducts from "../hooks/useproducts";
+
+// export default function Slider() {
+//   const{ products, isLoading, error } = useproducts("limit=5&skip=30");
+
+
+//   return (
+//     <div>
+//       {isLoading ? "Loading..." : "" }
+//       {error}
+//       <Swiper
+//         spaceBetween={50}
+//         slidesPerView={3}
+//         modules={[Navigation, Pagination, Scrollbar, Autoplay, A11y]}
+//         autoplay={{ delay: 2000, pauseOnMouseEnter: true }}
+//         pagination={{ clickable: true }}
+//         navigation
+//         scrollbar={{ draggable: true }}
+//         onSlideChange={() => console.log("slide change")}
+//         onSwiper={(swiper) => console.log(swiper)}
+//       >
+        
+//         {products?.slice(0,6)?.map((item)=>(
+//         <SwiperSlide key={item.id}>
+//           <ProductCard 
+//           id={item.id}
+//           image={item.thumbnail}
+//           name={item.title}
+//           price={item.price}
+//           discountPercentage={item.discountPercentage}
+//           rating={item.rating}
+//         />
+//       </SwiperSlide>
+    
+//     ))}
+
+//     </Swiper>
+//     </div>
+//   );
+// }
+
+
+
+
+
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -12,16 +71,16 @@ import {
   Autoplay,
 } from "swiper/modules";
 import ProductCard from "./ProductCard";
-import useproducts from "../hooks/useproducts";
+import useProducts from "../hooks/useProducts";
 
 export default function Slider() {
-  const{ products, isLoading, error } = useproducts("limit=5&skip=30");
-
+  const { products, isLoading, error } = useProducts("?limit=5&skip=30");
 
   return (
     <div>
-      {isLoading ? "Loading..." : "" }
+      {isLoading ? "loading..." : ""}
       {error}
+
       <Swiper
         spaceBetween={50}
         slidesPerView={3}
@@ -30,25 +89,22 @@ export default function Slider() {
         pagination={{ clickable: true }}
         navigation
         scrollbar={{ draggable: true }}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log("slide change")}
+        // onSwiper={(swiper) => console.log(swiper)}
       >
-        
-        {products?.slice(0,6)?.map((item)=>(
-        <SwiperSlide key={item.id}>
-          <ProductCard 
-          id={item.id}
-          image={item.thumbnail}
-          name={item.title}
-          price={item.price}
-          discountPercentage={item.discountPercentage}
-          rating={item.rating}
-        />
-      </SwiperSlide>
-    
-    ))}
-
-    </Swiper>
+        {products?.slice(0, 6)?.map((item) => (
+          <SwiperSlide key={item.id}>
+            <ProductCard
+              id={item.id}
+              image={item.thumbnail}
+              name={item.title}
+              price={item.price}
+              discountPercentage={item.discountPercentage}
+              rating={item.rating}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 }
--- Ecommerce project/src/pages/Search.jsx
+++ Ecommerce project/src/pages/Search.jsx
@@ -1,30 +1,30 @@
-import { useState } from "react";
 import ProductCard from "../components/ProductCard";
-import useproducts from "../hooks/useproducts";
+import useProducts from "../hooks/useProducts";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 
 export default function Search() {
   const darkMode = useSelector((state) => state.darkMode.darkMode);
 
-
   const [searchParams] = useSearchParams();
-  const searchTerm = searchParams.get("query")
-  
+  const searchTerm = searchParams.get("query");
 
-  const{ products,isLoading, error } = useproducts();
-  
+  const { products, isLoading, error } = useProducts(
+    `/search?q=${searchTerm}&limit=50`,
+  );
 
   return (
-    <div className={`container-x transition-colors duration-300 ${darkMode ?"bg-slate-900":""}`}>
-     
-
+    <div
+      className={`container-x transition-colors duration-300 ${
+        darkMode ? "bg-slate-900" : ""
+      } `}
+    >
       {products === null ? "Loading...." : null}
 
       <div className="flex flex-wrap justify-center gap-4">
-        {searchKaResult.length === 0 ? "product not found" : ""}
+        {products?.length === 0 ? "product not found" : ""}
 
-        {searchKaResult?.map((item) => (
+        {products?.map((item) => (
           <ProductCard
             darkMode={darkMode}
             id={item.id}
